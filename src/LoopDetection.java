ccaa3a772558811fadde66f86f71a887a329a927
sahil <sahil311289@gmail.com>
2018-05-16 12:19:57 -0700

    LinkedList

--- src/LoopDetection.java
+++ src/LoopDetection.java
@@ -12,7 +12,7 @@ public class LoopDetection {
         Node slow = head;
         Node fast = head;
 
-        while(fast != null) {
+        while(fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
 
