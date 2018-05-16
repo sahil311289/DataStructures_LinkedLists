fee048bdaa98d97d017bbd49047225755cfe7356
sahil <sahil311289@gmail.com>
2018-05-16 11:51:10 -0700

    LinkedList

--- src/Node.java
+++ src/Node.java
@@ -19,6 +19,27 @@ public class Node {
         return head;
     }
 
+    static Node constructLL2() {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+        head.next.next.next.next.next.next = new Node(7);
+
+        return head;
+    }
+
+    static Node constructLL3() {
+        Node head = new Node(24);
+        head.next = new Node(5);
+        head.next.next = new Node(6);
+        head.next.next.next = new Node(7);
+
+        return head;
+    }
+
     static void displayLL(Node head) {
         Node temp = head;
         while(temp != null) {
